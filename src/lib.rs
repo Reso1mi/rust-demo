64ba1eaa3b8c01ef77abffc85f2853913330ae3c
resolmi <privateli@qq.com>
2025-07-27 10:51:34 +0800

    fix: bin

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+// src/lib.rs
+pub fn greet() {
+    println!("Hello from the library!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
 #![allow(unused)]
-mod demo;
-// use demo::struct_r::User;
+use utils::greet;
 fn main() {
-    // demo::hello_world::main();
-    // demo::function::main();
-    // demo::ownership::main();
-    // demo::slice::main();
-    // demo::variable::main();
-    // demo::struct_r::main();
-    // demo::match_r::main();
-    // let user = User{active: true, name:String::from("123"), email:String::from("123"), sign_in_count:1};
-    // demo::vector_1::main();
-    // demo::vector_2::main();
-    // demo::hashmap::main();
-    // demo::error_r::main();
-    // demo::ownership_2::main();
-    // demo::trait_1::main();
-    demo::lifetime_1::main();
+    greet();
 }
