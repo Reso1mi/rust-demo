da165b084438126b188645aab1708c8b68ad2a10
resolmi <privateli@qq.com>
2025-07-28 23:27:04 +0800

    Update trait_1.rs

--- src/bin/trait_1.rs
+++ src/bin/trait_1.rs
@@ -90,6 +90,28 @@ where
 //     }
 // }
 
+// dyn trait
+fn returns_summarizable(switch: bool) -> Box<dyn Summary> {
+    if switch {
+        Box::from(NewsArticle {
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best
+            hockey team in the NHL.",
+            ),
+        })
+    } else {
+        Box::from(Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        })
+    }
+}
+
 fn largest<T: PartialOrd>(list: &Vec<T>) -> &T {
     let mut max = &list[0];
     // 模式匹配
