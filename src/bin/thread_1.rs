74901acf59628ce79d6b9de6ab946251a735a66e
Reso1mi <tadowli@tencent.com>
2025-08-08 18:08:55 +0800

    feat: barrier

--- src/bin/thread_1.rs
+++ src/bin/thread_1.rs
@@ -1,8 +1,46 @@
-use std::{thread, time::Duration};
+use std::sync::Arc;
+use std::{sync::Barrier, thread, time::Duration};
 
 fn main() {
     // thread_spawn();
-    thread_spawn_move();
+    // thread_spawn_move();
+    // child_child_thread();
+    thread_barrier();
+}
+
+fn thread_barrier() {
+    let mut handles = vec![];
+    let barrier = Arc::new(Barrier::new(5));
+    for i in 1..6 {
+        let b = barrier.clone();
+        handles.push(thread::spawn(move || {
+            println!("thread {i} before barrier");
+            b.wait();
+            println!("thread {i} after barrier");
+        }));
+    }
+
+    handles.into_iter().for_each(|h| {
+        h.join();
+    });
+}
+
+fn child_child_thread() {
+    // 创建一个线程A
+    let new_thread = thread::spawn(move || {
+        // 再创建一个线程B
+        thread::spawn(move || {
+            loop {
+                println!("I am a new thread.");
+            }
+        })
+    });
+
+    // 等待新创建的线程执行完成
+    new_thread.join().unwrap();
+    println!("Child thread is finish!");
+
+    thread::sleep(Duration::from_millis(100));
 }
 
 fn thread_spawn_move() {
