46634de29e41a914635cba3888d7498b9884314f
Resolmi <privateli@qq.com>
2025-08-27 22:28:14 +0800

    web server

--- src/bin/web_server.rs
+++ src/bin/web_server.rs
@@ -1,4 +1,10 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{BufRead, BufReader, Error, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 
 fn main() {
     let addr = "127.0.0.1:6789";
@@ -8,8 +14,40 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => println!("conn success!"),
+            Ok(stream) => match handle_conn(stream) {
+                Err(e) => eprintln!("handle error: {e:?}"),
+                _ => {}
+            },
             Err(e) => eprintln!("conn error {e:?}"),
         }
     }
+}
+
+fn handle_conn(mut stream: TcpStream) -> Result<(), Error> {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
+
+    let request_line = &http_request[0];
+
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "src/static/index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "src/static/index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "src/static/404.html"),
+    };
+
+    let contents = fs::read_to_string(filename)?;
+    let length = contents.len();
+
+    let response = format!("{status_line} \r\nContent-Length: {length}\r\n\r\n\n{contents}");
+
+    stream.write_all(response.as_bytes())?;
+    Ok(())
 }
